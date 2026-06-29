05e6c5d0263a595bb495d45ac6cf73aa6213682e
mickael_dutra <mickael.santos@cwi.com.br>
2026-06-29 08:05:21 -0300

    add: mover zeroes de um array

---
+++ two pointers/01-mover zeros/exercicio/src/Main.java
@@ -0,0 +1,20 @@
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+//Note that you must do this in-place without making a copy of the array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int [] nums = {0,1,0,3,12};
+        solution.moveZeroes(nums);
+    }
+}
+
+
---
+++ two pointers/01-mover zeros/exercicio/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroes = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0){
+                nums[nonZeroes++] = nums[i];
+            }
+        }
+
+        for (int i = nonZeroes; i < nums.length; i++){
+            nums[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
