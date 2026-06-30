261ff440ceed78c5173603eddaedf5ed2a2897fd
mickael_dutra <mickael.santos@cwi.com.br>
2026-06-30 07:56:05 -0300

    revisao sliding window

--- sliding window/01-maior media em um subarray/exercicio/src/Main.java
+++ sliding window/01-maior media em um subarray/exercicio/src/Main.java
@@ -15,7 +15,7 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1,12,-5,-6,50,3,4,5};
+        int[] nums = {1,12,-5,-6,50,3};
         int k = 4;
         System.out.println(solution.findMaxAverage(nums, k));
     }
--- sliding window/02-maior substring de vogais/exercicio/src/Solution.java
+++ sliding window/02-maior substring de vogais/exercicio/src/Solution.java
@@ -1,30 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public boolean isVowel(char c) {
+    private boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 
     public int maxVowels(String s, int k) {
         int maxVowel = 0;
         int left = 0;
-        int vowel = 0;
+        int currentVowel = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            if (isVowel(s.charAt(right))) {
-                vowel++;
-            }
-            if ((right - left + 1) == k) {
-                maxVowel = Math.max(maxVowel, vowel);
+            char characterRight = s.charAt(right);
+            char characterLeft = s.charAt(left);
 
-                if (isVowel(s.charAt(left))) {
-                    vowel--;
+            if (isVowel(characterRight)) {
+                currentVowel++;
+                maxVowel = Math.max(maxVowel, currentVowel);
+            }
+
+            if (right - left + 1 == k) {
+                if (isVowel(characterLeft)) {
+                    currentVowel--;
                 }
                 left++;
             }
         }
+
         return maxVowel;
     }
+
 }
 
 
